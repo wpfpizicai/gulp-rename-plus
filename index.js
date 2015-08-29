84173f930266de0313dcbcf709b1e72bf8a99c0b
yuheng.wpf <yuheng.wpf@alibaba-inc.com>
2015-08-29 23:58:43 +0800

    change by  wangpengfei

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
-/**
- * Created by patrickliu on 15/5/28.
- */
-var through = require('through2')
-    , gutil = require('gulp-util')
-    , crypto = require('crypto')
-    , extend = require('node.extend')
-    , fs = require('fs')
-    , path = require('path')
-    , glob = require('glob');
+var path = require('path')
+, gutil = require('gulp-util')
+, through = require('through2')
+, crypto = require('crypto')
+, fs = require('fs')
+, glob = require('glob');
 
 
 module.exports = function(options) {
 
-    var defaults = {
-        hashFunction: function(filename) {
-
-            return function(content) {
-                return filename.split('.').map(function(item, i, arr) {
-                    return i == arr.length - 1 ? 'ejs' : item
-                }).join('.');
-            }
-        }
-    };
-
-    options = extend(defaults, options);
-
-    return through.obj(function(file, enc, cb) {
-
+    return through.obj(function (file, enc, cb) {
         if (file.isStream()) {
             this.emit('error', new gutil.PluginError('gulp-debug', 'Streaming not supported'));
             return cb();
@@ -36,21 +18,29 @@ module.exports = function(options) {
             return cb();
         }
 
-        var dirName = path.dirname(file.path),
-            fileName = path.basename(file.path),
-            hashFunction = options.hashFunction(fileName),
-            fileContents = file.contents.toString('utf-8');
+        var filename = path.basename(file.path)
+        , relativepath = path.relative(file.base ,file.path)
+        , sub_namepath = relativepath.replace(new RegExp(filename) , "").split(path.sep).join('/')
+        , dir;
 
-        file.path = path.join(dirName, hashFunction(fileContents));
+        if(file.path[0] == '.'){
+            dir = path.join(file.base, file.path);
+        } else {
+            dir = file.path;
+        }
+        dir = path.dirname(dir);
+
+        var _filename = filename.split('.').map(function(item, i, arr){
+            return i == arr.length-1 ? options.suffix : item;
+        }).join('.');
+
+
+        file.path = path.join(dir, _filename);
 
         this.push(file);
+        cb();
+    }, function (cb) {
         cb();
     });
 }
 
-function calcMd5(file){
-    var md5 = crypto.createHash('md5');
-    md5.update(file, 'utf8');
-
-    return  md5.digest('hex').slice(0, 8);
-}
